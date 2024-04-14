3dfab93a406c58c69b3427afb2791dca962c94a7
Christopher Landaverde <christopher4780@gmail.com>
2024-04-13 20:05:26 -0500

    Attempt 1

---
+++ recipe.js
@@ -0,0 +1,16 @@
+// script.js
+function openRecipe(recipeName) {
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+        tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(recipeName).style.display = "block";
+}
+
+// Default open first tab
+document.getElementById("Recipe1").style.display = "block";
